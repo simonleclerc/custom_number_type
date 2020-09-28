2b6a4a4d08062d4727647a9ebd884b64ddeb19eb
Simon LE CLERC <slc.le.clerc@gmail.com>
2020-09-28 21:10:43 +0200

    feat: it increments the number and return the array of digit

--- src/increment.test.ts
+++ src/increment.test.ts
@@ -1,9 +1,9 @@
 import { increment } from './increment';
 
 describe('increment', () => {
-  it('increment the number and return it', () => {
+  it('increment the number and return the array of digit', () => {
     const result = increment(['9','0','1']);
 
-    expect(result).toEqual(902);
+    expect(result).toEqual(['9','0', '2']);
   });
 });
--- src/increment.ts
+++ src/increment.ts
@@ -2,5 +2,7 @@ export const increment = (inputs: string[]) => {
   return inputs
     .reverse()
     .map((input, i) => parseInt(input) * Math.pow(10, i))
-    .reduce((acc, val) => acc + val, 1);
+    .reduce((acc, val) => acc + val, 1)
+    .toString()
+    .split('');
 };
