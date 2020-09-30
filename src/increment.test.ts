b19c7b30aa5a8cafeb859035342cb0ff88095b0e
Simon LE CLERC <simon@gojob.com>
2020-09-30 21:48:17 +0200

    refacto: lint

--- src/increment.test.ts
+++ src/increment.test.ts
@@ -2,8 +2,8 @@ import { increment } from './increment';
 
 describe('increment', () => {
   it('increment the number and return the array of digit', () => {
-    const result = increment(['9','0','1']);
+    const result = increment(['9', '0', '1']);
 
-    expect(result).toEqual(['9','0', '2']);
+    expect(result).toEqual(['9', '0', '2']);
   });
 });
